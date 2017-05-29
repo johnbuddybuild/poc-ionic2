d1b6d8109f77559f9db60ff48650710ae27a566d
Christina Kaskoura <christina.kaskoura@gmail.com>
2017-05-29 12:11:24 +0300

    Added initial buddybuild_postbuild to execute functional tests

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,2 +1,12 @@
-npm run test:func
-cat ./output/functional/test.json | ./node_modules/.bin/cucumber-junit-enhance > testresults/xmloutput.xml
+echo 'Built app is' $BUDDYBUILD_IPA_PATH
+dirname $BUDDYBUILD_IPA_PATH
+
+#Only run functional tests on debug scheme
+if [ "$BUDDYBUILD_SCHEME" != "MyApp - Debug" ]; then
+  echo 'Scheme is not debug - skipping functional tests'
+else
+  echo 'Executing functional tests'
+  npm run test:func
+  #Covert Cucumber JSON output to JUnit XML
+  cat ./output/functional/test.json | ./node_modules/.bin/cucumber-junit-enhance > testresults/xmloutput.xml
+fi
